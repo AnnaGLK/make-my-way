d853c5b66414f344632983b23133be2fb345bc00
Anna Glikin <glikin.anna@gmail.com>
2025-09-26 02:15:42 +0300

    favicon loader

--- make-my-way-frontend/src/components/LoadingPopup.jsx
+++ make-my-way-frontend/src/components/LoadingPopup.jsx
@@ -1,12 +1,13 @@
 // src/components/LoadingPopup.jsx
 import React from "react"
 import "../styles/LoadingPopup.css"
+import Favicon from "../assets/favicon.png"
 
 export default function LoadingPopup() {
     return (
         <div className="loading-overlay">
             <img
-                src="../assets/favicon.png"
+                src={Favicon}
                 alt="Loading..."
                 className="loading-icon"
             />
